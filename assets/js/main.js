0811f06cc667135db86105fe05ee0d3b5a9a9371
g0tten <g0tten@yahoo.com>
2026-07-11 04:46:57 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,17 @@
   $(function(){
     $(".button-collapse").sideNav();
 
+    $(".drawer-close").on("click", function(e){
+      e.preventDefault();
+      $(".button-collapse").sideNav("hide");
+    });
+
+    $(document).on("keydown", function(e){
+      if (e.key === "Escape" || e.keyCode === 27) {
+        $(".button-collapse").sideNav("hide");
+      }
+    });
+
     var path = window.location.pathname.replace(/index\.html$/, "");
     if (path === "") {
       path = "/";
